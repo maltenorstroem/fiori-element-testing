1fe25a554929acda994b9b6d1f0b56fed0d5d0e1
Malte Norström <malte.norstroem@gmail.com>
2020-04-24 11:13:27 +0200

    Initial commit

---
+++ src/views/view-auth.js
@@ -0,0 +1,110 @@
+import { LitElement, html, css } from 'lit-element';
+import { Theme } from '@furo/framework/src/theme.js';
+import { FBP } from '@furo/fbp';
+import '@furo/form/src/furo-button-bar.js';
+
+/**
+ * `view-auth`
+ * todo Describe your element
+ *
+ * @summary todo shortdescription
+ * @customElement
+ * @demo demo-view-auth
+ * @appliesMixin FBP
+ */
+class ViewAuth extends FBP(LitElement) {
+  /**
+   * @private
+   * @return {Object}
+   */
+  static get properties() {
+    return {
+      /**
+       * Description
+       */
+      myBool: { type: Boolean },
+    };
+  }
+
+  /**
+   * flow is ready lifecycle method
+   */
+  _FBPReady() {
+    super._FBPReady();
+    // this._FBPTraceWires()
+  }
+
+  /**
+   * Themable Styles
+   * @private
+   * @return {CSSResult}
+   */
+  static get styles() {
+    // language=CSS
+    return (
+      Theme.getThemeForComponent(this.name) ||
+      css`
+        :host {
+          display: block;
+          height: 100vh;
+        }
+
+        :host([hidden]) {
+          display: none;
+        }
+        furo-card {
+          width: 360px;
+        }
+
+        furo-form-layouter > * {
+          margin: 20px 0;
+        }
+      `
+    );
+  }
+
+  /**
+   * @private
+   * @returns {TemplateResult}
+   * @private
+   */
+  render() {
+    // language=HTML
+    return html`
+      <furo-vertical-flex>
+        <furo-empty-spacer></furo-empty-spacer>
+        <furo-horizontal-flex>
+          <furo-empty-spacer></furo-empty-spacer>
+          <furo-card header-text="Login" secondary-text="Enter the username and password">
+            <furo-form-layouter one>
+              <furo-text-input
+                leading-icon="account-box"
+                autofocus
+                hint="Your name or email"
+                label="Username"
+                value="demo"
+              ></furo-text-input>
+              <furo-password-input
+                leading-icon="lock-outline"
+                value="1234"
+                hint="Look under your keyboard or on postit below monitor"
+                label="super secret password"
+                ƒ-toggle-visibility="--togglePasswordClicked"
+                trailing-icon="visibility"
+                @-trailing-icon-clicked="--togglePasswordClicked"
+              ></furo-password-input>
+            </furo-form-layouter>
+            <furo-horizontal-flex space="" slot="action">
+              <furo-button primary unelevated>Login</furo-button>
+              <furo-empty-spacer></furo-empty-spacer>
+            </furo-horizontal-flex>
+          </furo-card>
+          <furo-empty-spacer></furo-empty-spacer>
+        </furo-horizontal-flex>
+        <furo-empty-spacer></furo-empty-spacer>
+      </furo-vertical-flex>
+    `;
+  }
+}
+
+window.customElements.define('view-auth', ViewAuth);
---
+++ src/views/view-dashboard.js
@@ -0,0 +1,135 @@
+import { LitElement, html, css } from 'lit-element';
+import { i18n, Theme } from '@furo/framework/src/furo.js';
+import { FBP } from '@furo/fbp';
+
+import '@furo/layout/src/furo-vertical-flex.js';
+import '@furo/form';
+import '@furo/input';
+import '@furo/app/src/furo-card.js';
+import '@furo/form/src/furo-form.js';
+import '@furo/input/src/furo-icon-button.js';
+import '@furo/notification/src/furo-snackbar.js';
+
+/**
+ * `view-dashboard`
+ * todo Describe your element
+ *
+ * @summary todo shortdescription
+ * @customElement
+ * @demo demo-view-dashboard
+ * @appliesMixin FBP
+ */
+class ViewDashboard extends FBP(LitElement) {
+  /**
+   * @private
+   * @return {Object}
+   */
+  static get properties() {
+    return {
+      /**
+       * Description
+       */
+      myBool: { type: Boolean },
+    };
+  }
+
+  /**
+   * flow is ready lifecycle method
+   */
+  _FBPReady() {
+    super._FBPReady();
+    // this._FBPTraceWires()
+  }
+
+  /**
+   * Themable Styles
+   * @private
+   * @return {CSSResult}
+   */
+  static get styles() {
+    // language=CSS
+    return (
+      Theme.getThemeForComponent(this.name) ||
+      css`
+        :host {
+          display: block;
+          height: 100%;
+          overflow: hidden;
+        }
+
+        :host([hidden]) {
+          display: none;
+        }
+
+        .content {
+          padding: var(--spacing-s);
+          background-color: var(--background);
+          color: var(--on-surface);
+        }
+
+        furo-card {
+          margin: 0 var(--spacing) var(--spacing) 0;
+          width: 280px;
+          float: left;
+        }
+      `
+    );
+  }
+
+  /**
+   * @private
+   * @returns {TemplateResult}
+   * @private
+   */
+  render() {
+    // language=HTML
+    return html`
+      <furo-vertical-flex>
+        <furo-app-bar-top drawer="main-drawer">
+          <div>Dashboard</div>
+          <furo-empty-spacer></furo-empty-spacer>
+          <furo-icon-button icon="search" @-click="--float"></furo-icon-button>
+          <furo-icon-button icon="settings" @-click="--op"></furo-icon-button>
+          <furo-snackbar
+            position-right
+            action-button-text="ignore"
+            timeout-in-ms="8000"
+            label-text="Settings page is not implemented yet. This message destroys itself in 8 seconds"
+            ƒ-show="--op"
+          ></furo-snackbar>
+        </furo-app-bar-top>
+        <div flex scroll class="content">
+          <furo-card class="selected">
+            <img src="assets/images/hamburg.jpg" slot="media" alt="" />
+            <h1>${i18n.t('untranslated text')}</h1>
+            <p>${i18n.t('lorem')}</p>
+          </furo-card>
+
+          <furo-card disabled>
+            <img src="assets/images/hamburg.jpg" slot="media" alt="" />
+            <h1>App-flow...</h1>
+            <p>Routing example with app-flow</p>
+            <furo-app-flow event="form-requested" ƒ-trigger="--form"></furo-app-flow>
+            <furo-horizontal-flex slot="action">
+              <furo-button primary ƒ-focus="--pageActivated" @-click="--form">register</furo-button>
+            </furo-horizontal-flex>
+          </furo-card>
+
+          <furo-card>
+            <h1>Simulate unauthorized</h1>
+            <p>Click on the button to emmit a unauthorized event.</p>
+            <p>
+              The unathorized event is catched in the app-shell and triggers an app-flow event which
+              causses the router to navigate to the <strong>/auth</strong> page..
+            </p>
+            <furo-horizontal-flex slot="action">
+              <furo-button danger @-click="^^unauthorized">Unauth</furo-button>
+            </furo-horizontal-flex>
+          </furo-card>
+        </div>
+      </furo-vertical-flex>
+    `;
+  }
+}
+
+window.customElements.define('view-dashboard', ViewDashboard);
